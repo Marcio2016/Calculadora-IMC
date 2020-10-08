cb658c119640fa34d3ab40dcfdf474bbd4bd887f
marcio <marcio.ferreira@cialne.com.br>
2020-10-08 17:16:51 -0300

    Implementação do calculo

--- lib/main.dart
+++ lib/main.dart
@@ -12,41 +12,97 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = "Informe seus dados!";
+
+  void _resetField() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus daods";
+    });
+  }
+
+  void _calculo() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso ${imc.toStringAsPrecision(3)}";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal ${imc.toStringAsPrecision(3)}";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso ${imc.toStringAsPrecision(3)}";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I ${imc.toStringAsPrecision(3)}";
+      } else if (imc >= 39.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II ${imc.toStringAsPrecision(3)}";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III ${imc.toStringAsPrecision(3)}";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh),
-          onPressed: () {},)
-        ],
-      ),
-      backgroundColor: Colors.white38,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline,size: 120.0,color:Colors.green[400]),
-          TextField(keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            labelText: "Peso (kg)",
-            labelStyle: TextStyle(color: Colors.greenAccent)
+        appBar: AppBar(
+          title: Text("Calculadora IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetField,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white24,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120.0, color: Colors.green[400]),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.greenAccent)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25),
+                controller: weightController,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.greenAccent)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25),
+                controller: heightController,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Container(
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: _calculo,
+                      child: Text('Calcular',
+                          style: TextStyle(fontSize: 25, color: Colors.white)),
+                      color: Colors.green,
+                    )),
+              ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25),
+              ),
+            ],
           ),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.green,fontSize: 25.0),
-          ),
-          TextField(keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            labelText: "Altura (cm)",
-            labelStyle: TextStyle(color: Colors.greenAccent)
-          ),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.green,fontSize: 25.0),
-          )
-        ],),
-    );
+        ));
   }
 }
-
